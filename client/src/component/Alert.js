4cb6605f6429d764c2d1ff5c003567e71f6d4868
YP Yoo <ypy1324@gmail.com>
2023-11-17 13:11:02 -0500

    css border change

--- client/src/component/Alert.js
+++ client/src/component/Alert.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "../style/Alert.css";
+import moment from "moment";
 
 function Alert(props) {
   const currentDate = new Date();
@@ -14,6 +15,7 @@ function Alert(props) {
           return compareDates < 6 && compareDates > -5 ? (
             <div key={i}>
               <div>{item.name}</div>
+              <div>{moment(item.expiryDate).add(1, "days").format("ll")}</div>
               <hr />
             </div>
           ) : null;
